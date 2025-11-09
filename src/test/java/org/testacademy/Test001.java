e5a38dbe8f895bfdd03f2480525b7089924a4f20
shubhamrauthan <shubhamrauthan48@gmail.com>
2025-11-09 17:03:11 +0530

    Initial commit

---
+++ src/test/java/org/testacademy/Test001.java
@@ -0,0 +1,9 @@
+package org.testacademy;
+
+import io.restassured.RestAssured;
+
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
