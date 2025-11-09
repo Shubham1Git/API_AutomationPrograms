64fd7d946825c157e6c74c280ef6d82be102c405
shubhamrauthan <shubhamrauthan48@gmail.com>
2025-11-09 17:17:35 +0530

    GET method added

--- src/test/java/org/testacademy/Test001.java
+++ src/test/java/org/testacademy/Test001.java
@@ -5,5 +5,21 @@ import io.restassured.RestAssured;
 public class Test001 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("REST Assured Test Case");
+        System.out.println("GET Request Demo");
+
+        // Gherkin
+        // Given() -> url, headers, body, payloads
+        // When -> http methods
+        // Then -> Verify the response (using assertion)
+
+        RestAssured.
+                given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking/1").log().all()
+                .when()
+                .get()
+                .then().log().all()
+                .statusCode(200);
     }
 }
